c73d9a1b342579b51fa9553348dcd47b5f16a054
chintan <chintan.patel@toptal.com>
2022-02-26 12:43:11 -0500

    First commit of Springboot Project

---
+++ spring-demo-examplke/src/main/java/com/springexample/springdemoexamplke/SpringDemoExamplkeApplication.java
@@ -0,0 +1,13 @@
+package com.springexample.springdemoexamplke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoExamplkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoExamplkeApplication.class, args);
+	}
+
+}
---
+++ spring-demo-examplke/src/test/java/com/springexample/springdemoexamplke/SpringDemoExamplkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexample.springdemoexamplke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoExamplkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
